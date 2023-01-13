647dfbb813dc3839a0bcdf668db9bd4ae24321f4
Ruth Aja <ruthajalah1@gmail.com>
2023-01-13 09:49:33 +0700

    Fix widht 100vw horizontal

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,10 +1,13 @@
 import React from "react";
+import { FeaturedProducts } from "../../components/FeaturedProducts/FeaturedProducts";
 import { Slider } from "../../components/Slider/Slider";
 
 export const Home = () => {
     return (
         <div className="home">
             <Slider />
+            <FeaturedProducts type="featured" />
+            <FeaturedProducts type="trending" />
         </div>
     );
 };
