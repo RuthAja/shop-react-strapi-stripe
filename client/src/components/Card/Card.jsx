fce3628e9908e8cab59bd7261e85898418756004
Ruth Aja <ruthajalah1@gmail.com>
2023-01-16 09:33:14 +0700

    add Card component

--- client/src/components/Card/Card.jsx
+++ client/src/components/Card/Card.jsx
@@ -7,11 +7,17 @@ export const Card = ({ item }) => {
         <Link className="link" to={`/product/${item.id}`}>
             <div className="card">
                 <div className="image">
+                    {item.isNew && <span>New Season</span>}
+                    {/* jika ada isNew
+                    maka span keluar */}
                     <img src={item.img} alt="" className="mainImg" />
+                    {/* {item.img2 && (
+                        <img src={item.img2} alt="" className="secondImg" />
+                    )} */}
                     <img src={item.img2} alt="" className="secondImg" />
                 </div>
                 <h2>{item.title}</h2>
-                <div className="proces">
+                <div className="prices">
                     <h3>${item.oldPrice}</h3>
                     <h3>${item.price}</h3>
                 </div>
