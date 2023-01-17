83d7407e330b15f1391be69fa1f3f0756d51d01a
Ruth Aja <ruthajalah1@gmail.com>
2023-01-17 10:39:10 +0700

    add components categories

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Categories } from "../../components/Categories/Categories";
 import { FeaturedProducts } from "../../components/FeaturedProducts/FeaturedProducts";
 import { Slider } from "../../components/Slider/Slider";
 
@@ -7,6 +8,7 @@ export const Home = () => {
         <div className="home">
             <Slider />
             <FeaturedProducts type="featured" />
+            <Categories />
             <FeaturedProducts type="trending" />
         </div>
     );
