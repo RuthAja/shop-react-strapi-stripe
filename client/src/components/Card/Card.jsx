3f89ed0121132bd9af8569e1e91f9a3ff8ae9bc8
Ruth Aja <ruthajalah1@gmail.com>
2023-02-10 16:09:57 +0700

    add API

--- client/src/components/Card/Card.jsx
+++ client/src/components/Card/Card.jsx
@@ -7,19 +7,33 @@ export const Card = ({ item }) => {
         <Link className="link" to={`/product/${item.id}`}>
             <div className="card">
                 <div className="image">
-                    {item.isNew && <span>New Season</span>}
+                    {item?.attributes.isNew && <span>New Season</span>}
                     {/* jika ada isNew
                     maka span keluar */}
-                    <img src={item.img} alt="" className="mainImg" />
+                    <img
+                        src={
+                            process.env.REACT_APP_UPLOAD_URL +
+                            item?.attributes?.img?.data?.attributes?.url
+                        }
+                        alt=""
+                        className="mainImg"
+                    />
                     {/* {item.img2 && (
                         <img src={item.img2} alt="" className="secondImg" />
                     )} */}
-                    <img src={item.img2} alt="" className="secondImg" />
+                    <img
+                        src={
+                            process.env.REACT_APP_UPLOAD_URL +
+                            item?.attributes?.img2?.data?.attributes?.url
+                        }
+                        alt=""
+                        className="secondImg"
+                    />
                 </div>
-                <h2>{item.title}</h2>
+                <h2>{item?.attributes.title}</h2>
                 <div className="prices">
-                    <h3>${item.oldPrice}</h3>
-                    <h3>${item.price}</h3>
+                    <h3>${item.oldPrice || item?.attributes.price + 20}</h3>
+                    <h3>${item?.attributes.price}</h3>
                 </div>
             </div>
         </Link>
